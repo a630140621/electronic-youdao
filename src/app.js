480e970b20820603a74748a34e50d3cc60874291
lovecrazy <630140621@qq.com>
2018-07-29 22:29:13 +0800

    修改默认窗口大小

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ let win
 function createWindow() {
     // 创建浏览器窗口。
     win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 800,
         show: false,
         // frame: false,
         autoHideMenuBar: true, // 自动隐藏菜单栏, 按 alt 键显示
